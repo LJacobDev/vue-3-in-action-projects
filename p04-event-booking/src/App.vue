58a4e86758cd2f1c041b3e34b55af011aae3d07e
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-10-06 19:10:53 -0400

    Recreated p04 from scratch again as exercise at setting up Vue project with tailwindcss

---
+++ p04-event-booking/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl">Event Booking App</h1>
+    <h2 class="text-2xl font-medium">All Events</h2>
+    <h2 class="text-2xl font-medium">Your Bookings</h2>
+  </main>
+</template>
