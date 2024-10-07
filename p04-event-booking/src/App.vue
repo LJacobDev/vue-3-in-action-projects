41f5f42e22b8b79e33759f3c85e33059853df7b1
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-10-06 22:11:50 -0400

    Add examples of props and events in EvendCard.vue

--- p04-event-booking/src/App.vue
+++ p04-event-booking/src/App.vue
@@ -2,7 +2,16 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <EventCard />
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard
+        v-for="i in 8"
+        :key="i"
+        title="Vue Conference 2024"
+        when="Sept 1 2024"
+        description="Learn and become Vue"
+        @register="console.log('registered!')"
+      />
+    </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
 </template>
--- p04-event-booking/src/components/EventCard.vue
+++ p04-event-booking/src/components/EventCard.vue
@@ -1,3 +1,19 @@
 <template>
-    <div>Component</div>
+  <div class="border border-gray-300 bg-white rounded-md">
+    <h3 class="p-4 font-medium text-xl border-b border-gray-300">{{ title }}</h3>
+    <p class="p-4 border-b border-gray-300">{{ when }}</p>
+    <p class="p-4">{{ description }}</p>
+    <section class="flex justify-end p-4">
+      <button @click="$emit('register')" class="text-sm font-medium border border-gray-300 rounded-full px-3 py 1 hover:bg-gray-100">Register</button>
+    </section>
+  </div>
 </template>
+
+<script setup>
+defineProps({
+  title: String,
+  when: String,
+  description: String
+})
+defineEmits(['register'])
+</script>
