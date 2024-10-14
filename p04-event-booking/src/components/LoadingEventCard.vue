5194a125921074886591f28212b7aab2ac991dd8
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-10-14 13:48:26 -0400

    Add placeholder visual for loading state with pulse animation

--- p04-event-booking/src/components/LoadingEventCard.vue
+++ p04-event-booking/src/components/LoadingEventCard.vue
@@ -1,3 +1,16 @@
+<script setup>
+import RoundedCard from '@/components/RoundedCard.vue'
+</script>
+
 <template>
-    <div>Loading Events...</div>
+  <RoundedCard class="animate-pulse">
+    <div class="p-4 bg-gray-300 border-b border-gray-200 h-8"></div>
+    <div class="p-4 bg-gray-300 border-b border-gray-200h-6"></div>
+    <div class="p-4 bg-gray-300 h-6"></div>
+    
+    <div class="flex justify-end p-4">
+        <div class="h-8 w-20 rounded-full bg-gray-300"></div>
+    </div>
+
+  </RoundedCard>
 </template>
