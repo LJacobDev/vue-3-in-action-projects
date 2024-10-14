e7a8ab019c0358dabe075ce225101493fd5c2618
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-10-14 12:21:49 -0400

    Add a loading indicator for user feedback while data is being fetched

--- p04-event-booking/src/App.vue
+++ p04-event-booking/src/App.vue
@@ -2,27 +2,30 @@
 import EventCard from '@/components/EventCard.vue'
 import BookingItem from '@/components/BookingItem.vue'
 
-import { onMounted, ref } from 'vue';
-const events = ref([]);
+import { onMounted, ref } from 'vue'
+const events = ref([])
+const eventsLoading = ref(false)
 
 const fetchEvents = async () => {
-  const response = await fetch("http://localhost:3001/events");
-  // console.log(response);
-  events.value = await response.json();
-  // console.log(events.value);
+  try {
+    eventsLoading.value = true
+    const response = await fetch('http://localhost:3001/events')
+    events.value = await response.json()
+  } finally {
+    eventsLoading.value = false
+  }
 }
 
-onMounted(() =>{
-  fetchEvents();
+onMounted(() => {
+  fetchEvents()
 })
-
 </script>
 
 <template>
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8">
+    <section v-if="!eventsLoading" class="grid grid-cols-2 gap-8">
       <EventCard
         v-for="event in events"
         :key="event.id"
@@ -32,6 +35,7 @@ onMounted(() =>{
         @register="console.log('registered!')"
       />
     </section>
+    <section v-else>Loading Events...</section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
       <BookingItem v-for="i in 3" :key="i" label="Vue Conference 2024" />
