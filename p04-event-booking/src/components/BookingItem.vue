f3e7ccefc9122045863ba1d128d77361f6a371ea
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-10-16 19:33:49 -0400

    Added 'optimistic UI' changes to booking resitration from memory after watching implementation a few hours eariler

--- p04-event-booking/src/components/BookingItem.vue
+++ p04-event-booking/src/components/BookingItem.vue
@@ -2,14 +2,18 @@
 import SectionedCard from '@/components/SectionedCard.vue'
 import RoundButton from '@/components/RoundButton.vue'
 defineProps({
-  label: String
+  label: String,
+  status: String
 })
 </script>
 
 <template>
   <SectionedCard>
     <div class="flex justify-between">
-      <div>{{ label }}</div>
+      <div class="flex space-x-4">
+        <div>{{ label }}</div>
+        <div>{{ status }}</div>
+      </div>
       <RoundButton variant="danger">Cancel</RoundButton>
     </div>
   </SectionedCard>
