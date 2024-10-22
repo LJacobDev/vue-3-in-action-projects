7c64a51dc9c7a63682493236d729adee3ef07406
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-10-22 14:48:21 -0400

    Extracted Event List functionality from App.vue into EventList.vue

---
+++ p04-event-booking/src/components/EventList.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="grid md:grid-cols-2 grid-cols-1 gap-8">
+    <template v-if="!eventsLoading">
+      <EventCard
+        v-for="event in events"
+        :key="event.id"
+        :title="event.title"
+        :when="event.date"
+        :description="event.description"
+        @register="$emit('registerBooking', event)"
+      />
+    </template>
+    <template v-else>
+      <LoadingEventCard v-for="i in 4" :key="i" />
+    </template>
+  </section>
+</template>
+
+<script setup>
+defineEmits(['registerBooking'])
+
+import EventCard from '@/components/EventCard.vue'
+import LoadingEventCard from '@/components/LoadingEventCard.vue'
+
+import { ref, onMounted } from 'vue'
+
+const events = ref([])
+const eventsLoading = ref(false)
+
+const fetchEvents = async () => {
+  try {
+    eventsLoading.value = true
+    const response = await fetch('http://localhost:3001/events')
+    events.value = await response.json()
+  } finally {
+    eventsLoading.value = false
+  }
+}
+
+onMounted(() => {
+  fetchEvents()
+})
+</script>
