003bf398a0c0cacb36614e3593645631199fe7a7
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-10-22 14:38:35 -0400

    doing course out of order, added responsive design column feature

--- p04-event-booking/src/App.vue
+++ p04-event-booking/src/App.vue
@@ -109,7 +109,7 @@ const cancelBooking = async (booking) => {
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8">
+    <section class="grid md:grid-cols-2 grid-cols-1 gap-8">
       <template v-if="!eventsLoading">
         <EventCard
           v-for="event in events"
