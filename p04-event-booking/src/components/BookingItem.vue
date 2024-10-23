84a01c068e58c0597d70a82eca20d308573642b7
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-10-23 13:56:35 -0400

    Add lucide icons for pending and confirmed booking states

--- p04-event-booking/src/components/BookingItem.vue
+++ p04-event-booking/src/components/BookingItem.vue
@@ -1,11 +1,19 @@
 <script setup>
 import SectionedCard from '@/components/SectionedCard.vue'
 import RoundButton from '@/components/RoundButton.vue'
-defineProps({
+import { LoaderCircle, Check, Loader } from 'lucide-vue-next'
+
+import { computed } from 'vue'
+
+const props = defineProps({
   label: String,
   status: String
-});
-defineEmits(['cancelBooking']);
+})
+
+const pending = computed(() => props.status === 'pending')
+const icon = computed(() => pending.value ? LoaderCircle : Check)
+
+defineEmits(['cancelBooking'])
 </script>
 
 <template>
@@ -13,7 +21,7 @@ defineEmits(['cancelBooking']);
     <div class="flex justify-between">
       <div class="flex space-x-4">
         <div>{{ label }}</div>
-        <div>{{ status }}</div>
+        <div><component :is="icon" :class="{'animate-spin':pending}"/></div>
       </div>
       <RoundButton variant="danger" @click="$emit('cancelBooking')">Cancel</RoundButton>
     </div>
