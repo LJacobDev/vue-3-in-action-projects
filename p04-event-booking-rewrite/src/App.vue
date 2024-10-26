fb8c34d6b8316e00abcf7290feae5fe103a91462
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-10-26 14:56:10 -0400

    set up tailwindcss

--- p04-event-booking-rewrite/src/App.vue
+++ p04-event-booking-rewrite/src/App.vue
@@ -1,9 +1,6 @@
-<script setup>
-</script>
-
 <template>
-
-  Hello World
-
+  <div class="p-4 bg-gray-500">Hello World</div>
+  <div class="p-4 bg-gray-500 text-2xl">
+    When this appears as styled, tailwind is ready
+  </div>
 </template>
-
