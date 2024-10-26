49d80ba2c6666168c4af64789c866e57526e3fa4
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-10-26 18:56:04 -0400

    Initial styling of event details before making into a component set

--- p04-event-booking-rewrite/src/components/EventsList.vue
+++ p04-event-booking-rewrite/src/components/EventsList.vue
@@ -22,16 +22,19 @@ const fetchEvents = async url => {
 <template>
   <div v-for="event in events" :key="event.id">
     <div>
-      {{ event.eventName }}
+      <h3 class="text-2xl">{{ event.eventName }}</h3>
     </div>
     <div>
-      {{ event.when }}
+      <h3 class="text-2xl">{{ event.when }}</h3>
     </div>
     <div>
-      {{ event.description }}
+      <h3 class="text-xl">{{ event.description }}</h3>
     </div>
     <div>
-      {{ event.location }}
+      <h3 class="text-2xl">{{ event.location }}</h3>
     </div>
+    <button class="px-4 py-2 rounded-lg w-30 h-12 bg-green-200 font-thin">
+      Register
+    </button>
   </div>
 </template>
