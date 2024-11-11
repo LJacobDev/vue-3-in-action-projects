087ba76848474078bb9bff117306be9583813172
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-11-11 12:50:49 -0500

    Set up TabLinks to visually show which one is the currently active tab

--- p05-settings-page/src/App.vue
+++ p05-settings-page/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import TabLink from '@/components/TabLink.vue';
-import type { Tab, TabKey } from './types';
+import type { Tab, TabKey } from '@/types';
 import { ref } from 'vue';
 
 const currentTab = ref<TabKey>('General');
@@ -22,11 +22,17 @@ const tabs: Tab[] = [
 </script>
 
 <template>
-
+  <main class="max-w-2xl mx-auto px-4">
     Settings Page
-    <ul class="flex">
+    <nav class="font-medium text-center text-gray-500 border-b border-gray-200 mb-4">
+      <ul class="flex flex-wrap -mb-px">
         <li v-for="tab in tabs" :key="tab.key">
-            <TabLink :current-tab="currentTab" :tab="tab"></TabLink>
+          <TabLink 
+          :current-tab="currentTab" 
+          :tab="tab"
+          @click="currentTab = tab.key" />
         </li>
-    </ul>
+      </ul>
+    </nav>
+  </main>
 </template>
--- p05-settings-page/src/components/TabLink.vue
+++ p05-settings-page/src/components/TabLink.vue
@@ -1,20 +1,32 @@
 <script setup lang="ts">
 import type { Tab, TabKey } from '@/types';
-
+import { computed } from 'vue';
 
 //define props for what the current active tab is by name, and give it a tab type which will have a name for what this tab is called, and what this tab should display in the UI
-defineProps<{
-    currentTab: TabKey;
-    tab: Tab
+const props = defineProps<{
+  currentTab: TabKey;
+  tab: Tab;
 }>();
 
 //a click event will be emitted by this tab but it doesn't need a typed parameter so it gets an empty array
 defineEmits<{
-    click: []
-}>()
+  click: [];
+}>();
 
+const isActive = computed(() => {
+  return props.currentTab === props.tab.key;
+});
 </script>
 
 <template>
-    {{tab.display}}
+  <button
+    @click="$emit('click')"
+    :class="{
+      'p-4 border-b-2': true,
+      'border-b-blue-600 text-blue-600 ': isActive,
+      'border-transparent': !isActive,
+    }"
+  >
+    {{ tab.display }}
+  </button>
 </template>
