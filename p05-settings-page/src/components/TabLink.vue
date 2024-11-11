5de3fa35341b7f670a9509707dd0710e911bab36
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-11-10 20:31:21 -0500

    Created TabLink component with typescript generics style prop and emit definitions

---
+++ p05-settings-page/src/components/TabLink.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+//define props for what the current active tab is by name, as well as what name this tab is called, and what this tab should display in the UI
+defineProps<{
+    currentTab: string;
+    tab: {
+        name: string;
+        display: string;
+    }
+}>();
+
+//a click event will be emitted by this tab but it doesn't need a typed parameter so it gets an empty array
+defineEmits<{
+    click: []
+}>()
+
+</script>
+
+<template>
+    Link
+</template>
