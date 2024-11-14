eef3a7e25c8a980d29038ae18422e9f5178f20d7
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-11-13 21:42:56 -0500

    Add useSettings.ts composable for GeneralSettings interface and ref, connect ref to html inputs

--- p05-settings-page/src/components/GeneralSettings.vue
+++ p05-settings-page/src/components/GeneralSettings.vue
@@ -3,28 +3,28 @@
     <div>
         <div>
             <label>Name</label>
-            <input type="text">
+            <input type="text" v-model="general.name">
         </div>
         <div>
             <label>E-mail</label>
-            <input type="email">
+            <input type="email" v-model="general.email">
         </div>
         <div>
             <label>About Me</label>
-            <textarea></textarea>
+            <textarea v-model="general.about"></textarea>
         </div>
         <div>
             <label>Gender</label>
-            <input type="radio" name="gender" id="male" value="male">
+            <input type="radio" name="gender" id="male" value="male" v-model="general.gender">
             <label for="male">Male</label>
-            <input type="radio" name="gender" id="female" value="female">
+            <input type="radio" name="gender" id="female" value="female" v-model="general.gender">
             <label for="female">Female</label>
-            <input type="radio" name="gender" id="other" value="other">
+            <input type="radio" name="gender" id="other" value="other" v-model="general.gender">
             <label for="other">Other / Prefer Not To Say</label>
         </div>
         <div>
             <label>Country</label>
-            <select>
+            <select v-model="general.country">
                 <option>Canada</option>
                 <option>Germany</option>
                 <option>UK</option>
@@ -33,3 +33,12 @@
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+
+import { useSettings } from '@/composables/useSettings';
+
+
+    const { general } = useSettings();
+
+</script>
---
+++ p05-settings-page/src/composables/useSettings.ts
@@ -0,0 +1,23 @@
+import GeneralSettings from '@/components/GeneralSettings.vue';
+
+interface GeneralSettings {
+  name: string;
+  email: string;
+  about: string;
+  gender: string;
+  country: string;
+}
+
+import { ref } from 'vue';
+
+const general = ref<GeneralSettings>({
+  name: '',
+  email: '',
+  about: '',
+  gender: 'male',
+  country: 'Canada',
+});
+
+export function useSettings() {
+  return {general};
+}
