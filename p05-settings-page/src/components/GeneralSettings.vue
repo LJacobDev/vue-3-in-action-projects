98324704aa8e65dff99af58e32747ec3de3269cd
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-11-13 21:30:44 -0500

    Add basic html for general settings

--- p05-settings-page/src/components/GeneralSettings.vue
+++ p05-settings-page/src/components/GeneralSettings.vue
@@ -1,3 +1,35 @@
 <template>
-    <div>General</div>
+    <h2>General Settings</h2>
+    <div>
+        <div>
+            <label>Name</label>
+            <input type="text">
+        </div>
+        <div>
+            <label>E-mail</label>
+            <input type="email">
+        </div>
+        <div>
+            <label>About Me</label>
+            <textarea></textarea>
+        </div>
+        <div>
+            <label>Gender</label>
+            <input type="radio" name="gender" id="male" value="male">
+            <label for="male">Male</label>
+            <input type="radio" name="gender" id="female" value="female">
+            <label for="female">Female</label>
+            <input type="radio" name="gender" id="other" value="other">
+            <label for="other">Other / Prefer Not To Say</label>
+        </div>
+        <div>
+            <label>Country</label>
+            <select>
+                <option>Canada</option>
+                <option>Germany</option>
+                <option>UK</option>
+                <option>USA</option>
+            </select>
+        </div>
+    </div>
 </template>
