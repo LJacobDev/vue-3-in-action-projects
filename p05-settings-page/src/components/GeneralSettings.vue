fd776c7c5ad01c12e1ef9196f0c89393f036a85c
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-11-13 21:44:51 -0500

    Add form tag, set GeneralSettings with one root element so it can transition

--- p05-settings-page/src/components/GeneralSettings.vue
+++ p05-settings-page/src/components/GeneralSettings.vue
@@ -1,44 +1,46 @@
 <template>
+  <div>
     <h2>General Settings</h2>
     <div>
+      <form>
         <div>
-            <label>Name</label>
-            <input type="text" v-model="general.name">
+          <label>Name</label>
+          <input type="text" v-model="general.name" />
         </div>
         <div>
-            <label>E-mail</label>
-            <input type="email" v-model="general.email">
+          <label>E-mail</label>
+          <input type="email" v-model="general.email" />
         </div>
         <div>
-            <label>About Me</label>
-            <textarea v-model="general.about"></textarea>
+          <label>About Me</label>
+          <textarea v-model="general.about"></textarea>
         </div>
         <div>
-            <label>Gender</label>
-            <input type="radio" name="gender" id="male" value="male" v-model="general.gender">
-            <label for="male">Male</label>
-            <input type="radio" name="gender" id="female" value="female" v-model="general.gender">
-            <label for="female">Female</label>
-            <input type="radio" name="gender" id="other" value="other" v-model="general.gender">
-            <label for="other">Other / Prefer Not To Say</label>
+          <label>Gender</label>
+          <input type="radio" name="gender" id="male" value="male" v-model="general.gender" />
+          <label for="male">Male</label>
+          <input type="radio" name="gender" id="female" value="female" v-model="general.gender" />
+          <label for="female">Female</label>
+          <input type="radio" name="gender" id="other" value="other" v-model="general.gender" />
+          <label for="other">Other / Prefer Not To Say</label>
         </div>
         <div>
-            <label>Country</label>
-            <select v-model="general.country">
-                <option>Canada</option>
-                <option>Germany</option>
-                <option>UK</option>
-                <option>USA</option>
-            </select>
+          <label>Country</label>
+          <select v-model="general.country">
+            <option>Canada</option>
+            <option>Germany</option>
+            <option>UK</option>
+            <option>USA</option>
+          </select>
         </div>
+        <button class="btn-primary">Save</button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-
 import { useSettings } from '@/composables/useSettings';
 
-
-    const { general } = useSettings();
-
+const { general } = useSettings();
 </script>
