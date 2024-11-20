f0ce9cb67a78eb8367fd743baadfa38af721a804
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-11-20 16:03:34 -0500

    modify settings initializations to use a reusable generic initSettings function

--- p05-settings-page/src/composables/useSettings.ts
+++ p05-settings-page/src/composables/useSettings.ts
@@ -8,18 +8,24 @@ interface GeneralSettings {
 
 import { ref } from 'vue';
 
-const general = ref<GeneralSettings>((() => {
-  const storage = localStorage.getItem('general');
 
-  return storage ? JSON.parse(storage) : {
+const initSettings = <T>(key: string, defaults: T) => {
+  const storage = localStorage.getItem(key);
+
+  return storage !== null
+    ? JSON.parse(storage)
+    : defaults
+
+}
+
+const general = ref<GeneralSettings>(
+  initSettings<GeneralSettings>('general', {
     name: '',
     email: '',
     about: '',
     gender: 'male',
     country: 'Canada',
-  }
-
-})());
+  }));
 
 
 type Visibility = 'public' | 'private';
@@ -29,10 +35,12 @@ interface PrivacySettings {
   searchEngineIndexing: boolean;
 }
 
-const privacy = ref<PrivacySettings>({
-  visibility: 'private',
-  searchEngineIndexing: false,
-});
+const privacy = ref<PrivacySettings>(
+  initSettings<PrivacySettings>('privacy', {
+    visibility: 'private',
+    searchEngineIndexing: false,
+  })
+);
 
 
 interface NotificationsSettings {
@@ -40,10 +48,12 @@ interface NotificationsSettings {
   sms: boolean;
 }
 
-const notifications = ref<NotificationsSettings>({
-  email: false,
-  sms: false,
-})
+const notifications = ref<NotificationsSettings>(
+  initSettings<NotificationsSettings>('notifications', {
+    email: false,
+    sms: false,
+  })
+)
 
 const saveSettings = () => {
   localStorage.setItem('general', JSON.stringify(general.value));
